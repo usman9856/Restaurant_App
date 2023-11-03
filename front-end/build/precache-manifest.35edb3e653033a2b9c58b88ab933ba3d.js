50993915648d4f0598940d0a42e47a321afd5d3c
Usman Muhammad <um50765@gmail.com>
2023-11-03 10:03:55 +0500

    Adding build folder to push file

---
+++ front-end/build/precache-manifest.35edb3e653033a2b9c58b88ab933ba3d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4ed9ae9f875cf30a7dc",
+    "url": "/static/css/main.77014485.chunk.css"
+  },
+  {
+    "revision": "e4ed9ae9f875cf30a7dc",
+    "url": "/static/js/main.e4ed9ae9.chunk.js"
+  },
+  {
+    "revision": "728b5c2ab1beaa9a566f",
+    "url": "/static/js/1.728b5c2a.chunk.js"
+  },
+  {
+    "revision": "ad04404597f484a65c40",
+    "url": "/static/css/2.8888ed60.chunk.css"
+  },
+  {
+    "revision": "ad04404597f484a65c40",
+    "url": "/static/js/2.ad044045.chunk.js"
+  },
+  {
+    "revision": "df2316a0d7230fd44f79",
+    "url": "/static/js/runtime~main.df2316a0.js"
+  },
+  {
+    "revision": "7c8d991e8a2be6abd4986c3e00c35aec",
+    "url": "/static/media/facebook.7c8d991e.png"
+  },
+  {
+    "revision": "0634a5babf36db50e415356b348e3f30",
+    "url": "/static/media/instagram.0634a5ba.png"
+  },
+  {
+    "revision": "d1044b2527b5b74f5802cbb1fd100bd0",
+    "url": "/static/media/twitter.d1044b25.png"
+  },
+  {
+    "revision": "515fc584baa97d0aae930bc6161a864f",
+    "url": "/static/media/cart.515fc584.png"
+  },
+  {
+    "revision": "8feff93941a2425f1d7b98118625a17d",
+    "url": "/static/media/user_.8feff939.png"
+  },
+  {
+    "revision": "108883994d5845c69af9a6254bd9f91b",
+    "url": "/index.html"
+  }
+];
